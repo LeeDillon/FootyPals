dd5953f048247a334f2dc69913354961f0bca824
Lee-Dillon <lee.dillon1@outlook.com>
2023-03-24 15:56:54 +0000

    added search button, trying to get data into usable state

--- src/utils/api-table.js
+++ src/utils/api-table.js
@@ -1,40 +1,48 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+// Empty array to store result objects from API call
+var resultsArray = [];
 
-const apiTable = () => {
-  const [data, setData] = useState([]);
+const searchButton = document.getElementById("search-button");
+searchButton.onclick(getTableData);
 
-  useEffect(() => {
-    const options = {
-      method: "GET",
-      url: "https://api-football-v1.p.rapidapi.com/v3/standings",
-      params: { season: "2022", league: "39" },
-      headers: {
-        "X-RapidAPI-Key": "7ca1969ae9msh68d5bd0f9355080p110c06jsn199987ed9ba9",
-        "X-RapidAPI-Host": "api-football-v1.p.rapidapi.com",
-      },
-    };
+function getTableData() {
+  const options = {
+    method: "GET",
+    url: "https://api-football-v1.p.rapidapi.com/v3/standings",
+    params: { season: "2022", league: "39" },
+    headers: {
+      "X-RapidAPI-Key": "7ca1969ae9msh68d5bd0f9355080p110c06jsn199987ed9ba9",
+      "X-RapidAPI-Host": "api-football-v1.p.rapidapi.com",
+    },
+  };
 
-    axios
-      .request(options)
-      .then(function (response) {
-        console.log(response.data.response[0].league.standings);
-        setData(response.data.response[0].league.standings);
-      })
-      .catch(function (error) {
-        console.error(error);
-      });
-  }, []);
+  axios
+    .request(options)
+    .then(function (response) {
+      for (
+        let i = 0;
+        i < response.data.response[0].league.standings.length;
+        i++
+      ) {
+        const position = response.data.response[0].league.standings.rank;
+        const team = response.data.response[0].league.standings.team.name;
+        const played = response.data.response[0].league.standings.all.played;
+        const goalDiff = response.data.response[0].league.standings.goalsDiff;
+        const points = response.data.response[0].league.standings.points;
+        const rowData = {
+          position: position,
+          team: team,
+          played: played,
+          goalDiff: goalDiff,
+          points: points,
+        };
+        resultsArray.push(rowData);
+      }
+      console.log(resultsArray);
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+  
 
-  return (
-    <div className="App">
-      {data.map((standings) => (
-        <div key={standings}>
-          <li></li>
-        </div>
-      ))}
-    </div>
-  );
-};
-
-export default apiTable;
+}
+// export default apiTable;
